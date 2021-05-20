e76c19fd1cf49e9a5003ea98d458c7ccc09502cc
larisayushina <yushina.larisa@yandex.ru>
2021-05-20 19:20:59 +0300

    initial commit

--- module_2/test_registration.py
+++ module_2/test_registration.py
@@ -25,8 +25,6 @@ try:
     button.click()
 
     # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
 
     # находим элемент, содержащий текст
     welcome_text_elt = browser.find_element_by_tag_name("h1")
