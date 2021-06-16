727afddf59c57568d0c07dde42c36f2083fccad7
larisayushina <yushina.larisa@yandex.ru>
2021-06-16 20:16:38 +0300

    Homework 5 module

---
+++ module_5/conftest.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption("--language", action="store", default="en-GB",
+                     help="Select language")
+    parser.addoption("--browser_name", action="store", default="chrome",
+                     help="Select: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+    elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+    else:
+        raise pytest.UsageError("--browser_name unrecognized")
+    yield browser
+    browser.quit()
+
+
+@pytest.fixture(scope="session")
+def language(request):
+    return request.config.getoption("language")
